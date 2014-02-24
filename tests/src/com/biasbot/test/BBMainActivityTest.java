54c3a52ec038b37e7169d0ba0e76be231853cbab
Alexander Conrad <me@alexanderconrad.org>
2014-02-24 00:13:18 -0500

    renaming test project dir, adding initial stub test class

---
+++ tests/src/com/biasbot/test/BBMainActivityTest.java
@@ -0,0 +1,15 @@
+package com.biasbot.test;
+
+import com.biasbot.BBMainActivity;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+public class BBMainActivityTest extends
+    ActivityInstrumentationTestCase2<BBMainActivity> {
+
+  public BBMainActivityTest(Class<BBMainActivity> activityClass) {
+    super(activityClass);
+    // TODO Auto-generated constructor stub
+  }
+
+}
